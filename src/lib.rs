6fa33b789e2988ff832316a0795ca7327ffc999a
Dominik <d.kuen@cloudacy.com>
2023-07-22 12:19:42 +0200

    chore: use modules to generate an invoice

--- src/lib.rs
+++ src/lib.rs
@@ -1,211 +1,22 @@
 pub mod address;
+pub mod biller;
 pub mod contact;
+pub mod details;
 pub mod document;
+pub mod identification;
+pub mod invoice_recipient;
+pub mod order_reference;
 pub mod tax;
 
 use std::collections::HashMap;
 
 use rust_decimal::{Decimal, RoundingStrategy::MidpointAwayFromZero};
 
-use address::Address;
-use contact::Contact;
+use biller::Biller;
+use details::Details;
 use document::DocumentType;
+use invoice_recipient::InvoiceRecipient;
 use tax::{TaxCategory, TaxItem};
-
-pub enum FurtherIdentificationType {
-    ARA,
-    #[allow(non_camel_case_types)]
-    BBG_GZ,
-    Consolidator,
-    Contract,
-    DVR,
-    EORI,
-    ERSB,
-    FN,
-    FR,
-    HG,
-    Payer,
-    FASTNR,
-    VID,
-    VN,
-}
-
-impl FurtherIdentificationType {
-    fn as_str(&self) -> &str {
-        match self {
-            FurtherIdentificationType::ARA => "ARA",
-            FurtherIdentificationType::BBG_GZ => "BBG_GZ",
-            FurtherIdentificationType::Consolidator => "Consolidator",
-            FurtherIdentificationType::Contract => "Contract",
-            FurtherIdentificationType::DVR => "DVR",
-            FurtherIdentificationType::EORI => "EORI",
-            FurtherIdentificationType::ERSB => "ERSB",
-            FurtherIdentificationType::FN => "FN",
-            FurtherIdentificationType::FR => "FR",
-            FurtherIdentificationType::HG => "HG",
-            FurtherIdentificationType::Payer => "Payer",
-            FurtherIdentificationType::FASTNR => "FASTNR",
-            FurtherIdentificationType::VID => "VID",
-            FurtherIdentificationType::VN => "VN",
-        }
-    }
-}
-
-pub struct FurtherIdentification<'a> {
-    pub id: &'a str,
-    pub id_type: FurtherIdentificationType,
-}
-
-impl FurtherIdentification<'_> {
-    fn as_xml(&self) -> String {
-        let id = self.id;
-        let id_type = self.id_type.as_str();
-        format!(
-            "<FurtherIdentification IdentificationType=\"{id_type}\">{id}</FurtherIdentification>"
-        )
-    }
-}
-
-pub struct OrderReference<'a> {
-    pub order_id: &'a str,
-    pub reference_date: Option<&'a str>,
-    pub description: Option<&'a str>,
-}
-
-impl OrderReference<'_> {
-    fn as_xml(&self) -> String {
-        let order_id = self.order_id;
-        let reference_date = match self.reference_date {
-            Some(d) => format!("<ReferenceDate>{d}</ReferenceDate>"),
-            None => format!(""),
-        };
-        let description = match self.description {
-            Some(d) => format!("<Description>{d}</Description>"),
-            None => format!(""),
-        };
-        format!("<OrderReference><OrderID>{order_id}</OrderID>{reference_date}{description}</OrderReference>")
-    }
-}
-
-pub struct Biller<'a> {
-    pub vat_identification_number: &'a str,
-    pub further_identification: Vec<FurtherIdentification<'a>>,
-    pub order_reference: Option<OrderReference<'a>>,
-    pub address: Option<Address<'a>>,
-    pub contact: Option<Contact<'a>>,
-}
-
-impl Biller<'_> {
-    fn as_xml(&self) -> String {
-        let vat_identification_number = self.vat_identification_number;
-        let further_identification_vec: Vec<String> = (&self.further_identification)
-            .into_iter()
-            .map(|id| id.as_xml())
-            .collect();
-        let further_identification = further_identification_vec.join("");
-        let order_reference = match &self.order_reference {
-            Some(order_reference) => order_reference.as_xml(),
-            None => format!(""),
-        };
-        let address = match &self.address {
-            Some(address) => address.as_xml(),
-            None => format!(""),
-        };
-        let contact = match &self.contact {
-            Some(contact) => contact.as_xml(),
-            None => format!(""),
-        };
-        format!("<Biller><VATIdentificationNumber>{vat_identification_number}</VATIdentificationNumber>{further_identification}{order_reference}{address}{contact}</Biller>")
-    }
-}
-
-pub struct InvoiceRecipient<'a> {
-    pub vat_identification_number: &'a str,
-    pub further_identification: Vec<FurtherIdentification<'a>>,
-    pub order_reference: Option<OrderReference<'a>>,
-    pub address: Option<Address<'a>>,
-    pub contact: Option<Contact<'a>>,
-}
-
-impl InvoiceRecipient<'_> {
-    fn as_xml(&self) -> String {
-        let vat_identification_number = self.vat_identification_number;
-        let further_identification_vec: Vec<String> = (&self.further_identification)
-            .into_iter()
-            .map(|id| id.as_xml())
-            .collect();
-        let further_identification = further_identification_vec.join("");
-        let order_reference = match &self.order_reference {
-            Some(order_reference) => order_reference.as_xml(),
-            None => format!(""),
-        };
-        let address = match &self.address {
-            Some(address) => address.as_xml(),
-            None => format!(""),
-        };
-        let contact = match &self.contact {
-            Some(contact) => contact.as_xml(),
-            None => format!(""),
-        };
-        format!("<InvoiceRecipient><VATIdentificationNumber>{vat_identification_number}</VATIdentificationNumber>{further_identification}{order_reference}{address}{contact}</InvoiceRecipient>")
-    }
-}
-
-pub struct DetailsItem<'a> {
-    pub position_number: Option<u64>,
-    pub description: Vec<&'a str>,
-    pub quantity: Decimal,
-    pub unit: &'a str,
-    pub unit_price: Decimal,
-    pub base_quantity: Option<Decimal>,
-    pub tax_item: TaxItem,
-}
-
-impl DetailsItem<'_> {
-    fn line_item_amount(&self) -> Decimal {
-        let base_quantity = match self.base_quantity {
-            Some(bq) => bq,
-            None => Decimal::ONE,
-        };
-        self.quantity * self.unit_price / base_quantity /* + sum of surcharge_list_line_item.amount + sum of other_vat_able_tax_list_line_item.tax_amount - sum of reduction_list_line_item.amount */
-    }
-
-    fn line_item_total_gross_amount(&self) -> Decimal {
-        self.line_item_amount()
-            * ((self.tax_item.tax_percent + Decimal::ONE_HUNDRED) / Decimal::ONE_HUNDRED)
-    }
-
-    fn as_xml(&self) -> String {
-        let position_number = match self.position_number {
-            Some(pn) => format!("<PositionNumber>{pn}</PositionNumber>"),
-            None => format!(""),
-        };
-        let description_vec: Vec<String> = (&self.description)
-            .into_iter()
-            .map(|d| format!("<Description>{d}</Description>"))
-            .collect();
-        let description = description_vec.join("");
-        let unit: &str = self.unit;
-        let base_quantity = match self.base_quantity {
-            Some(bq) => format!(" BaseQuantity=\"{bq}\""),
-            None => format!(""),
-        };
-        let tax_item_xml = self.tax_item.as_xml();
-        format!("<ListLineItem>{position_number}{description}<Quantity Unit=\"{unit}\">{:.4}</Quantity><UnitPrice{base_quantity}>{:.4}</UnitPrice>{tax_item_xml}<LineItemAmount>{:.2}</LineItemAmount></ListLineItem>", self.quantity.round_dp_with_strategy(4, MidpointAwayFromZero), self.unit_price.round_dp_with_strategy(4, MidpointAwayFromZero), self.line_item_amount().round_dp_with_strategy(2, MidpointAwayFromZero))
-    }
-}
-
-pub struct Details<'a> {
-    pub items: Vec<DetailsItem<'a>>,
-}
-
-impl Details<'_> {
-    fn as_xml(&self) -> String {
-        let items_xml_vec: Vec<String> = (&self.items).into_iter().map(|l| l.as_xml()).collect();
-        let items_xml = items_xml_vec.join("");
-        format!("<Details><ItemList>{items_xml}</ItemList></Details>")
-    }
-}
 
 pub fn generate(
     document_type: DocumentType,
@@ -265,6 +76,12 @@ pub fn generate(
 mod tests {
     use super::*;
     use rust_decimal_macros::dec;
+
+    use address::Address;
+    use contact::Contact;
+    use details::DetailsItem;
+    use identification::{FurtherIdentification, FurtherIdentificationType};
+    use order_reference::OrderReference;
 
     #[test]
     fn round_line_item_amount_result_after_calculation() {
