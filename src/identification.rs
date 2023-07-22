bff9b170a1d0a0d22b2786897a8bc857fdde0aa8
Dominik <d.kuen@cloudacy.com>
2023-07-22 12:18:56 +0200

    chore: move FurtherIdentification to identification module

---
+++ src/identification.rs
@@ -0,0 +1,53 @@
+pub enum FurtherIdentificationType {
+    ARA,
+    #[allow(non_camel_case_types)]
+    BBG_GZ,
+    Consolidator,
+    Contract,
+    DVR,
+    EORI,
+    ERSB,
+    FN,
+    FR,
+    HG,
+    Payer,
+    FASTNR,
+    VID,
+    VN,
+}
+
+impl FurtherIdentificationType {
+    pub fn as_str(&self) -> &str {
+        match self {
+            FurtherIdentificationType::ARA => "ARA",
+            FurtherIdentificationType::BBG_GZ => "BBG_GZ",
+            FurtherIdentificationType::Consolidator => "Consolidator",
+            FurtherIdentificationType::Contract => "Contract",
+            FurtherIdentificationType::DVR => "DVR",
+            FurtherIdentificationType::EORI => "EORI",
+            FurtherIdentificationType::ERSB => "ERSB",
+            FurtherIdentificationType::FN => "FN",
+            FurtherIdentificationType::FR => "FR",
+            FurtherIdentificationType::HG => "HG",
+            FurtherIdentificationType::Payer => "Payer",
+            FurtherIdentificationType::FASTNR => "FASTNR",
+            FurtherIdentificationType::VID => "VID",
+            FurtherIdentificationType::VN => "VN",
+        }
+    }
+}
+
+pub struct FurtherIdentification<'a> {
+    pub id: &'a str,
+    pub id_type: FurtherIdentificationType,
+}
+
+impl FurtherIdentification<'_> {
+    pub fn as_xml(&self) -> String {
+        let id = self.id;
+        let id_type = self.id_type.as_str();
+        format!(
+            "<FurtherIdentification IdentificationType=\"{id_type}\">{id}</FurtherIdentification>"
+        )
+    }
+}
